e3c4f4fc5cff2ae1448e7130f4df3851d131ff50
savan <savan56>
2026-06-14 11:10:25 +0530

    initial commit

---
+++ src/main/java/com/flexbenefits/FlexbenefitsApplication.java
@@ -0,0 +1,13 @@
+package com.flexbenefits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexbenefitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexbenefitsApplication.class, args);
+	}
+
+}
